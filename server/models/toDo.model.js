5d00a45366256f447232d4e714471ee4b5e33cb7
Justin E Ahlers <jusahlcoder@gmail.com>
2022-12-12 08:49:42 -0800

    getting there

--- server/models/toDo.model.js
+++ server/models/toDo.model.js
@@ -1,13 +1,10 @@
-//import mongoose to create mongoose model
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
-//create Schema
 const TodoItemSchema = new mongoose.Schema({
     item: {
         type: String,
         required: true,
-    },
-});
+    }
+})
 
-//export this Schema
 module.exports = mongoose.model("todo", TodoItemSchema)
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -3,15 +3,34 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: [true, "First name is required"]
+    },
+    lastName: {
+        type: String,
+        required: [true, "Last name is required"]
+    },
     email: {
         type: String,
-        required: true,
+        required: [true, "Email is required"],
         unique: true
     },
     password: {
         type: String,
-        required: true
+        required: [true, "Password is required"],
+        minlength: [8, "Password must be 8 characters or longer"]
     }
-})
+}, { timestamps: true });
+//     email: {
+//         type: String,
+//         required: true,
+//         unique: true
+//     },
+//     password: {
+//         type: String,
+//         required: true
+//     }
+// })
 
 module.exports = mongoose.model('User', userSchema)
